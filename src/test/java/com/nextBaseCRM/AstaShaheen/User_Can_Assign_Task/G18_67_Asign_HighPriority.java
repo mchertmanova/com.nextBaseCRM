b50f676d5284b985d4d076ed175eea8e0c7a78e1
Asta Shaheen <asta11004@yahoo.com>
2021-01-19 13:41:10 -0500

    Task1

--- src/test/java/com/nextBaseCRM/AstaShaheen/User_Can_Assign_Task/G18_67_Asign_HighPriority.java
+++ src/test/java/com/nextBaseCRM/AstaShaheen/User_Can_Assign_Task/G18_67_Asign_HighPriority.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class G18_67_Asign_HighPriority {
@@ -34,12 +35,48 @@ public class G18_67_Asign_HighPriority {
         password.sendKeys("UserUser", Keys.ENTER);
     }
 
+
+    // Click on "Task" module
+
     @Test
     public void taskTab() {
         login();
         WebElement taskTab = driver.findElement(By.linkText("Tasks"));
         taskTab.click();
     }
+
+
+   // From the task table click on any available task "name"
+    @Test
+    public void selectName(){
+        taskTab();
+
+//        List<WebElement> allRows= driver.findElements(By.xpath("//table[@class='main-grid-table']//tr"));
+//        List<WebElement> allCells= driver.findElements(By.xpath("//table[@class='main-grid-table']//tr//td"));
+//        for(WebElement each:allRows){
+//            System.out.println("Row "+each.getText());
+//        }
+//        for(WebElement each:allCells){
+//            System.out.println("Cell "+each.getText());
+//        }
+
+        WebElement firstSecection= driver.findElement(By.xpath("//div[@class='main-grid-container']//tr[1]//td[3]//a"));
+       firstSecection.click();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+
+        WebElement frame1= driver.findElement(By.xpath("//*[@class='side-panel-iframe']"));
+        driver.switchTo().frame(frame1);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        WebElement fireSignNotSelected= driver.findElement(By.xpath("//span[@class='if-no']"));
+        fireSignNotSelected.click();
+        WebElement fireSignIsSelected= driver.findElement(By.xpath("//span[@class='if-no'][text()='High Priority']"));
+        driver.switchTo().parentFrame();
+
+        driver.findElement(By.xpath("//span[@title='Close']")).click();
+        driver.navigate().refresh();
+
+}
 
 
 }
