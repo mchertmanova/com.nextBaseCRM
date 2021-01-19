4704a2a1be66ec46575eb20ce7200a0f2452a503
AsiaBizub <74011714+AsiaBizub@users.noreply.github.com>
2021-01-18 21:14:53 -0600

    commit

--- src/test/java/com/nextBaseCRM/AsiaBizub/UserStory_14_Logout.java
+++ src/test/java/com/nextBaseCRM/AsiaBizub/UserStory_14_Logout.java
@@ -2,3 +2,24 @@ package com.nextBaseCRM.AsiaBizub;
 
 public class UserStory_14_Logout {
 }
+
+/*
+User Story 14:
+As a user, I should be able to logout from the app
+
+Acceptance Criteria:
+Verify users can logout and return back to login page.
+
+Scenario:
+Users (hr, marketing and helpdesk) able to logout under user email
+
+Credentials:
+         UserName                 Password                QA
+helpdesk35@cybertekschool.com     UserUser      login2.nextbasecrm.com
+helpdesk36@cybertekschool.com     UserUser	    login2.nextbasecrm.com
+marketing35@cybertekschool.com    UserUser      login2.nextbasecrm.com
+marketing36@cybertekschool.com	  UserUser	    login2.nextbasecrm.com
+hr35@cybertekschool.com           UserUser      login2.nextbasecrm.com
+hr36@cybertekschool.com           UserUser      login2.nextbasecrm.com
+
+ */
